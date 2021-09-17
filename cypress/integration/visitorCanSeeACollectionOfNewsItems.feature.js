6f78e9f6855fd3b0dcabfef1673c88ae8de63ef9
Fraser <Fraser.Hughes@btinternet.com>
2021-09-17 19:52:59 +0200

    Refactored tests to work with cards

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -2,6 +2,7 @@ describe("The collection of news articles", () => {
   beforeEach(() => {
     cy.visit("/");
   });
+
   it("is expected for header to exist and have image", () => {
     cy.get("[data-cy=logo]").within(() => {
       cy.get("[data-cy=logoImg]").should("be.visible");
@@ -10,13 +11,17 @@ describe("The collection of news articles", () => {
 
   it("is expected to display a header", () => {
     cy.get("[data-cy=header]").should("contain", "News Wire Network");
-    cy.get("[data-cy=header-card]").should("contain", "News from around the world")
+    cy.get("[data-cy=header-card]").should(
+      "contain",
+      "News from around the world"
+    );
   });
 
   it("is expected to show a card of the top headline", () => {
-    cy.get("[data-cy=top-headline]").should("be.visible");
-    cy.get("[data-cy=top-headline]").within(() => {
-      cy.get("[data-cy=header").should("contain", "DBS Bank to launch");
+    cy.get("#news-1").within(() => {
+      cy.get(".image").find("img".should("have.attr"));
+      cy.get(".header").should("contain", "DBS Bank to launch cryptocurrency");
+      cy.get(".description").should("contain", "DBS Bank of Singapore has just announced");
     });
   });
 });
