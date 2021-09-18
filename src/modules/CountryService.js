010857823441c3e69aaae4eea970dc9ab296dba6
Fraser <Fraser.Hughes@btinternet.com>
2021-09-18 16:48:18 +0200

    added open cage api key to env file

--- src/modules/CountryService.js
+++ src/modules/CountryService.js
@@ -4,7 +4,7 @@ const CountryService = async (coords) => {
   const apiKey = '55082dd8fb7e473db8dab86a33518599';
   const { latitude, longitude } = coords;
 
-  const apiUrl = process.envREACT_APP_LOCATION_API_URL
+  const apiUrl = process.env.REACT_APP_LOCATION_API_URL
 
   const requestUrl = process.REACT_APP_LOCATION_API_KEY
   + '?'
