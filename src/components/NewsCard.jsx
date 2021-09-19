3b20b975d0500de4327fa5b71c7443b963fb6f58
Fraser <Fraser.Hughes@btinternet.com>
2021-09-19 08:20:28 +0200

    Makes news cards link to article URLs

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -3,14 +3,18 @@ import { Card, Header } from "semantic-ui-react";
 
 const NewsCard = ({ item }) => {
   return (
-    <Card
-      data-cy="news-card"
-      image={item.urlToImage}
-      header={item.title}
-      meta={`By: ${item.author} at ${item.source.name}`}
-      description={item.description}
-      extra={<Header as="h6">Published at {item.publishedAt}</Header>}
-    />
+    <a href={item.url}>
+      <button onClick="NewsCard">
+        <Card
+          data-cy="news-card"
+          image={item.urlToImage}
+          header={item.title}
+          meta={`By: ${item.author} at ${item.source.name}`}
+          description={item.description}
+          extra={<Header as="h6">Published at {item.publishedAt}</Header>}
+        />
+      </button>
+    </a>
   );
 };
 
